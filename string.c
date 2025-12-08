e8a196e1dd12a675c628c1c645e9ecec765274ed
askpatrik <ask.patrik@hotmail.com>
2025-12-08 21:27:10 +0100

    Initial commit

---
+++ string.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+
+char * name = "Patrik Ask"; 
+char name2[] = "Anna Widen";
+
+printf("Your names are %s and %s\n", name, name2); 
+
+printf("The length of Patrik Ask is %d\n", strlen(name));
+
+}
